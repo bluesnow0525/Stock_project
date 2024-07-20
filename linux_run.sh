0cc3b41e6a4b3fd525fe6a77914442ba11baab88
DennisLiu525 <dennis131414@gmail.com>
2024-07-21 01:19:33 +0800

    update

--- linux_run.sh
+++ linux_run.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
-# Define the Python interpreter paths
+# 定義 Python 解釋器的路徑
 LINUX_PYTHON="/path/to/linux/python"
 MACOS_PYTHON="/path/to/macos/python"
 
-# Function to detect the OS and run appropriate commands
+# 定義運行命令的函數
 run_commands() {
-    OS_NAME=$(uname)  # Capture the OS name
+    OS_NAME=$(uname)  # 捕獲操作系統名稱
     if [ "$OS_NAME" = "Linux" ]; then
-        # Linux specific commands
+        # Linux 特定的命令
         gnome-terminal -- sh -c './start_ngrok.sh; sleep 20; exec bash'
-        gnome-terminal -- sh -c 'sleep 20; $LINUX_PYTHON ./fetch_public_url.py && $LINUX_PYTHON ./Stock-backend/src/backend/main.py; exec bash'
+        gnome-terminal -- sh -c 'sleep 20 && '"$LINUX_PYTHON"' ./fetch_public_url.py && '"$LINUX_PYTHON"' ./Stock-backend/src/backend/main.py; exec bash'
+        gnome-terminal -- sh -c 'sleep 60 && cd Stock-frontend && npm run dev; exec bash'
     elif [ "$OS_NAME" = "Darwin" ]; then
-        # macOS specific commands
+        # macOS 特定的命令
         osascript -e 'tell app "Terminal" to do script "sh ./start_ngrok.sh; sleep 20"'
-        osascript -e 'tell app "Terminal" to do script "sleep 20; $MACOS_PYTHON ./fetch_public_url.py && $MACOS_PYTHON ./Stock-backend/src/backend/main.py"'
+        osascript -e 'tell app "Terminal" to do script "sleep 20 && '$MACOS_PYTHON' ./fetch_public_url.py && '$MACOS_PYTHON' ./Stock-backend/src/backend/main.py"'
+        osascript -e 'tell app "Terminal" to do script "sleep 60 && cd Stock-frontend && npm run dev"'
     else
-        echo "Unsupported OS: $OS_NAME"
+        echo "不支持的操作系統: $OS_NAME"
         exit 1
     fi
 }
 
-# Run the commands
+# 運行命令
 run_commands
--- windows_run.bat
+++ windows_run.bat
@@ -1,3 +1,4 @@
 @echo off
 start cmd.exe /k "sh ./start_ngrok.sh & timeout /t 20"
-start cmd.exe /k "timeout /t 20 & D:/DL/Documents/anaconda3/envs/StockEnv/python.exe ./fetch_public_url.py && D:/DL/Documents/anaconda3/envs/StockEnv/python.exe ./Stock-backend/src/backend/main.py"
+start cmd.exe /k "timeout /t 20 && D:/DL/Documents/anaconda3/envs/StockEnv/python.exe ./fetch_public_url.py && D:/DL/Documents/anaconda3/envs/StockEnv/python.exe ./Stock-backend/src/backend/main.py"
+start cmd.exe /k "timeout /t 60 && cd Stock-frontend && npm run dev"
