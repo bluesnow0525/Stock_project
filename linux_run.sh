26806d04672dfffe015275022956b7334b0817c7
DennisLiu525 <dennis131414@gmail.com>
2024-07-21 01:49:34 +0800

    update

--- linux_run.sh
+++ linux_run.sh
@@ -11,12 +11,12 @@ run_commands() {
         # Linux 特定的命令
         gnome-terminal -- sh -c './start_ngrok.sh; sleep 20; exec bash'
         gnome-terminal -- sh -c 'sleep 20 && '"$LINUX_PYTHON"' ./fetch_public_url.py && '"$LINUX_PYTHON"' ./Stock-backend/src/backend/main.py; exec bash'
-        gnome-terminal -- sh -c 'sleep 60 && cd Stock-frontend && npm run dev; exec bash'
+        gnome-terminal -- sh -c 'sleep 40 && cd Stock-frontend && npm run dev; exec bash'
     elif [ "$OS_NAME" = "Darwin" ]; then
         # macOS 特定的命令
         osascript -e 'tell app "Terminal" to do script "sh ./start_ngrok.sh; sleep 20"'
         osascript -e 'tell app "Terminal" to do script "sleep 20 && '$MACOS_PYTHON' ./fetch_public_url.py && '$MACOS_PYTHON' ./Stock-backend/src/backend/main.py"'
-        osascript -e 'tell app "Terminal" to do script "sleep 60 && cd Stock-frontend && npm run dev"'
+        osascript -e 'tell app "Terminal" to do script "sleep 40 && cd Stock-frontend && npm run dev"'
     else
         echo "不支持的操作系統: $OS_NAME"
         exit 1
