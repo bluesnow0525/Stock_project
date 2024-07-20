cb050e1cefdb3fd6b5e158c3a1e955a96bfc5fbd
DennisLiu525 <dennis131414@gmail.com>
2024-07-21 01:08:41 +0800

    update

--- fetch_public_url.py
+++ fetch_public_url.py
@@ -13,14 +13,16 @@ if response.status_code == 200:
     result = {tunnel['name']: tunnel['public_url'] for tunnel in tunnels}
     
     # 打印结果并写入文件
-    with open(r'./stock-backend/src/backend/public_urls.json', 'w') as file:
+    with open(r'./Stock-backend/src/backend/public_urls.json', 'w') as file:
         json.dump(result, file, indent=4)
         
     # # 打印结果并写入文件
     # with open(r'./stock-frontend/public/public_urls.json', 'w') as file:
     #     json.dump(result, file, indent=4)
-    with open(r'./stock-frontend/src/assets/apirul.tsx','w+') as file:
-        file.write("export const API_BASE_URL = 'https://aa76-118-161-170-14.ngrok-free.app';")
+    
+    # 修改前端文件
+    with open(r'./Stock-frontend/src/assets/apiurl.tsx','w+') as file:
+        file.write(f"export const API_BASE_URL = '{result['stock-backend']}';")
         
     # 打印结果
     for name, public_url in result.items():
