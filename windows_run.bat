e7fc354b5c2b0a01ee2bbbb975702f0bd0c6843f
DennisLiu525 <dennis131414@gmail.com>
2024-07-30 13:58:16 +0800

    update

--- windows_run.bat
+++ windows_run.bat
@@ -1,4 +1,3 @@
 @echo off
-start cmd.exe /k "sh ./start_ngrok.sh & timeout /t 20"
-start cmd.exe /k "timeout /t 20 && D:/DL/Documents/anaconda3/envs/StockEnv/python.exe ./fetch_public_url.py && D:/DL/Documents/anaconda3/envs/StockEnv/python.exe ./Stock-backend/src/backend/main.py"
-start cmd.exe /k "timeout /t 40 && cd Stock-frontend && npm run dev"
+start cmd.exe /k "sh ./start_ngrok.sh"
+start cmd.exe /k "timeout /t 20 && call start_backend_frontend.bat"
