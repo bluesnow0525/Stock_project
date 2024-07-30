6a72812dd01f92cd8c5892f1accca62ce2ed31aa
DennisLiu525 <dennis131414@gmail.com>
2024-07-30 18:19:49 +0800

    update

--- linux_run.sh
+++ linux_run.sh
@@ -3,6 +3,9 @@
 # Run the start_ngrok.sh script in a new terminal window
 gnome-terminal -- sh -c './start_ngrok.sh; exec bash'
 
+# Wait for 10 seconds
+sleep 10
+
 # Run the fetch_public_url.py script with the specified Python interpreter
 D:/DL/Documents/anaconda3/envs/StockEnv/python.exe ./fetch_public_url.py
 
